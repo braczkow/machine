bc6866c4f1a900d3da820f1693e0c99313339930
braczkow <braczkow@gmail.com>
2016-02-28 23:01:28 +0100

    double while in HB

--- HB.py
+++ HB.py
@@ -21,30 +21,32 @@ def HB_worker(config):
     
     print 'name: ' + name + ' platform_IP: ' + platform_IP + ' platform_port: ' + platform_port
     
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error, msg:
-        print 'HB socket create failed. Bye Bye.'
-        return
-
-    print 'HB socket created.' 
-    print 'HB waiting for platform connect...'
-    
-    s.connect((platform_IP , int(platform_port)))
-    
-    print 'HB platform connected.' 
-    
-    message = '{"name": "' + name + '"}'
-    print 'HB message: ' + message
-    
     while True:
-        print 'about to send heart-beat '
         try:
-            s.sendall(message)
-        except Exception:
-            print 'platform unreachable'
-            
-        sleep(1)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error, msg:
+            print 'HB socket create failed. Bye Bye.'
+            return
+    
+        print 'HB socket created.' 
+        print 'HB waiting for platform connect...'
+        
+        s.connect((platform_IP , int(platform_port)))
+        
+        print 'HB platform connected.' 
+        
+        message = '{"name": "' + name + '"}'
+        print 'HB message: ' + message
+        
+        while True:
+            print 'about to send heart-beat '
+            try:
+                s.sendall(message)
+            except Exception:
+                print 'platform unreachable'
+                break
+                
+            sleep(1)
 
 def start_HB(config):
     print 'starting heart-beat'
